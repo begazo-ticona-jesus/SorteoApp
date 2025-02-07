a46df29323d2f1e5b4e5c43e3fd412c50fd8c0c8
begazo-ticona-jesus <jbegazoti@unsa.edu.pe>
2025-02-06 23:51:41 -0500

    correcion de paquetes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://begazo-ticona-jesus.github.io/SorteoApp',
 })
